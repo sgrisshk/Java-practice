c03cc8029eb9131eb84779afe8016f9d0ac23190
Sofya Grishkova <144588966+niigheye@users.noreply.github.com>
2024-09-05 20:53:48 +0200

    refactor(common): add game name in ASCII art style

--- laba1/src/Game.java
+++ laba1/src/Game.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
+//creating the name of the game in an ASCII art style
 public class Game{
     private static Scanner scanner;
     public static void main(String[] args){
         scanner=new Scanner(System.in);
         // ----- Write your code below
+        System.out.println("       __                    _          ___       __                 __                \n" +
+                "      / /__  ____________  _( )_____   /   | ____/ /   _____  ____  / /___  __________ \n" +
+                " __  / / _ \\/ ___/ ___/ / / /// ___/  / /| |/ __  / | / / _ \\/ __ \\/ __/ / / / ___/ _ \\\n" +
+                "/ /_/ /  __/ /  / /  / /_/ / (__  )  / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __/\n" +
+                "\\____/\\___/_/  /_/   \\__, / /____/  /_/  |_\\__,_/ |___/\\___/_/ /_/\\__/\\__,_/_/   \\___/ \n" +
+                "                    /____/                                                            ");
+        System.out.println("\nby: Sofya Grishkova");
         // ----- Write your code above
     }
 }
