7ef0349a77416235037965a76418de32bf0f3692
Sofya Grishkova <144588966+sgrisshk@users.noreply.github.com>
2024-09-05 12:02:44 +0200

    Add files via upload

---
+++ laba1/src/Game.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Game{
+    private static Scanner scanner;
+    public static void main(String[] args){
+        scanner=new Scanner(System.in);
+        // ----- Write your code below
+
+        // ----- Write your code above
+    }
+}
