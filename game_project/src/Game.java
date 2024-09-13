95d440599f02cf56cfc031eae5078d7219cc299e
Sofya Grishkova <144588966+niigheye@users.noreply.github.com>
2024-09-13 15:59:48 +0200

    feat(commom): add new methods, rename the game
    add getInput method, rename a firstAction method to takeAction; write new story text parts; rename the game

--- game_project/src/Game.java
+++ game_project/src/Game.java
@@ -19,69 +19,90 @@ public class Game {
         System.out.println("You are " + age + " years old.");
     }
 
-    public static void firstAction() {
+
+    public static String getInput() {
         System.out.println("\nWhat would you like to do?:");
         scanner.nextLine();
-        String action = scanner.nextLine();
-        if (Objects.equals(action, "Open the door")){
-            System.out.println("\nOpen the door");
+        String action = scanner.nextLine().toLowerCase();
+        if (action.equals("quit")){
+            System.exit(0);
         }
-        else{
-            System.out.println("\nYou can't do this");
+        else {
+            if (action.equals("open the door") || action.equals("go north") || action.equals("go south") || action.equals("go east") || action.equals("go west") || action.equals("take item") || action.equals("drop item") || action.equals("use item")) {
+                return action;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+        return null;
+    }
+    public static void takeAction(String action, int currentState) {
+        switch(action){
+            case "open the door": currentState = 1; break;
+            case "take them": currentState = 2; break;
+            case "go east": currentState = 3; break;
+            default: currentState = 0;break;
+        }
+    }
+
+    public static void printState(int stateId){
+        switch (stateId){
+            case 0: System.out.println("You decide to inspect the ceiling, hoping to find\n " +
+                    "an air vent or hidden passage. Your eyes scan the cracked tiles above,\n " +
+                    "but all you notice is the faint hum of the flickering lights.\n " +
+                    "There's no secret escape, just the same looming choices ahead.\n " +
+                    "The corridor feels colder now, as if urging you to make a real decision.\n"); break;
+
+            case 1: System.out.println("You step through the metallic door and into a dim corridor,\n " +
+                    "where the walls are lined with cracked, flickering lights. In the distance,\n " +
+                    "you hear mechanical sounds, like the clank of metal against metal.\n " +
+                    "To your east, a control panel flickers with life.\n " +
+                    "To the north, you sense a faint draft.\n " +
+                    "You can feel the weight of the unknown pressing down on you.\n " +
+                    "The way back to the control room is to the south.\n"); break;
+
+            case 2: System.out.println("You grab the radio from the desk, wiping away the dust.\n " +
+                    "As you hold it, you hear static followed by a broken transmission:\n " +
+                    "\"If you... hear this... get to the east side... the only safe... is...\"\n " +
+                    "The signal cuts out. You now hold the radio in your hand, hoping it might help you\n " +
+                    "survive in this unforgiving world.\n"); break;
+            case 3: System.out.println(
+                                                    "As you move east, the air feels colder,\n " +
+                                                    "and the lights dim even more. You reach a room \n" +
+                                                    "filled with blinking screens and a large map of the area.\n" +
+                                                    "A man stands near the map, dressed in a lab coat and frantically\n " +
+                                                    "scribbling notes. His eyes meet yours, filled with both fear and hope.\n " +
+                                                    "Before you can speak, he says, \"You have to help us... The system is failing...\n " +
+                                                    "We’re running out of time.\" Behind him, the map shows the last known safe zones,\n " +
+                                                    "each one crossed out.\n "); break;
         }
     }
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-
-        //first variant of the title
-       /* System.out.println("       __                    _          ___       __                 __                \n" +
-                "      / /__  ____________  _( )_____   /   | ____/ /   _____  ____  / /___  __________ \n" +
-                " __  / / _ \\/ ___/ ___/ / / /// ___/  / /| |/ __  / | / / _ \\/ __ \\/ __/ / / / ___/ _ \\\n" +
-                "/ /_/ /  __/ /  / /  / /_/ / (__  )  / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __/\n" +
-                "\\____/\\___/_/  /_/   \\__, / /____/  /_/  |_\\__,_/ |___/\\___/_/ /_/\\__/\\__,_/_/   \\___/ \n" +
-                "                    /____/                                                            "); */
-
-        //preferable variant of the title
-        System.out.println(" " +
-                "_____                                 __               ______      __                         __                           \n" +
-                "/\\___ \\                               /\\ \\             /\\  _  \\    /\\ \\                       /\\ \\__                        \n" +
-                "\\/__/\\ \\     __   _ __   _ __   __  __\\ \\/      ____   \\ \\ \\L\\ \\   \\_\\ \\  __  __     __    ___\\ \\ ,_\\  __  __  _ __    __   \n" +
-                "   _\\ \\ \\  /'__`\\/\\`'__\\/\\`'__\\/\\ \\/\\ \\\\/      /',__\\   \\ \\  __ \\  /'_` \\/\\ \\/\\ \\  /'__`\\/' _ `\\ \\ \\/ /\\ \\/\\ \\/\\`'__\\/'__`\\ \n" +
-                "  /\\ \\_\\ \\/\\  __/\\ \\ \\/ \\ \\ \\/ \\ \\ \\_\\ \\      /\\__, `\\   \\ \\ \\/\\ \\/\\ \\L\\ \\ \\ \\_/ |/\\  __//\\ \\/\\ \\ \\ \\_\\ \\ \\_\\ \\ \\ \\//\\  __/ \n" +
-                "  \\ \\____/\\ \\____\\\\ \\_\\  \\ \\_\\  \\/`____ \\     \\/\\____/    \\ \\_\\ \\_\\ \\___,_\\ \\___/ \\ \\____\\ \\_\\ \\_\\ \\__\\\\ \\____/\\ \\_\\\\ \\____\\\n" +
-                "   \\/___/  \\/____/ \\/_/   \\/_/   `/___/> \\     \\/___/      \\/_/\\/_/\\/__,_ /\\/__/   \\/____/\\/_/\\/_/\\/__/ \\/___/  \\/_/ \\/____/\n" +
-                "                                    /\\___/                                                                                  \n" +
-                "                                    \\/__/                                                                                   ");
-
-        //third variant of the title
-       /* System.out.println("  ____    ___  ____   ____   __ __  __  _____      ____  ___    __ __    ___  ____   ______  __ __  ____     ___ \n" +
-                " |    |  /  _]|    \\ |    \\ |  T  TT  |/ ___/     /    T|   \\  |  T  |  /  _]|    \\ |      T|  T  T|    \\   /  _]\n" +
-                " l__  | /  [_ |  D  )|  D  )|  |  |l_ (   \\_     Y  o  ||    \\ |  |  | /  [_ |  _  Y|      ||  |  ||  D  ) /  [_ \n" +
-                " __j  |Y    _]|    / |    / |  ~  |  \\l\\__  T    |     ||  D  Y|  |  |Y    _]|  |  |l_j  l_j|  |  ||    / Y    _]\n" +
-                "/  |  ||   [_ |    \\ |    \\ l___, |    /  \\ |    |  _  ||     |l  :  !|   [_ |  |  |  |  |  |  :  ||    \\ |   [_ \n" +
-                "\\  `  ||     T|  .  Y|  .  Y|     !    \\    |    |  |  ||     | \\   / |     T|  |  |  |  |  l     ||  .  Y|     T\n" +
-                " \\____jl_____jl__j\\_jl__j\\_jl____/      \\___j    l__j__jl_____j  \\_/  l_____jl__j__j  l__j   \\__,_jl__j\\_jl_____j\n" +
-                "                                                                                                                 ");*/
+       System.out.println("┏┳┓┓     ┏┓┓            ┓  ┏┓   ┓ \n" +
+               " ┃ ┣┓┏┓  ┗┓┣┓┏┓╋╋┏┓┏┓┏┓┏┫  ┃┃┏┓╋┣┓\n" +
+               " ┻ ┛┗┗   ┗┛┛┗┗┻┗┗┗ ┛ ┗ ┗┻  ┣┛┗┻┗┛┗\n");
 
         System.out.println("\nby: Sofya Grishkova");
 
+        int currentState=0;
         getName();
         calculateAge();
 
 
-       System.out.println("You are standing in an abandoned university office. There are neither students\n" +
-                "nor teachers around you. There's a table in front of you with various papers,\n" +
-                "pens, a small puzzle toy, and a calculator.\n" +
-                "A large window shows an empty office building; there are no Zombies in the empty\n" +
-                "building (as far as you can tell). Behind you is a dark and mysterious door that\n" +
-                "leads to a well-lit corridor with a fireproof ceiling and floor. You feel a\n" +
-                "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be\n" +
-                "heard in the distance. You are not thirsty, but you rather have a craving for\n" +
-                "justice.");
+        System.out.println(
+                "You find yourself in a dimly lit control room\n " +
+                "filled with flashing lights and monitors showing live feeds\n " +
+                "of desolate streets. The air is thick with tension,\n " +
+                "and the distant hum of machines fills your ears. On a desk nearby,\n " +
+                "there’s a cryptic note and an old, dust-covered radio.\n " +
+                "A large metallic door to the south hums with energy.\n " +
+                "You sense that danger is close, but you're unsure\n" +
+                "from where it will come.\n");
 
-        firstAction();
-
-
+        String stateId = getInput();
+        takeAction(stateId, currentState);
+        printState(currentState);
     }
 }
